9baa5c5e8823f7d8f0ec52be0831be3f01746fdb
ningyinjun <1103380684@qq.com>
2019-09-07 15:18:00 +0800

    添加test工程至git

---
+++ idea-git-test/src/java/cn/itcast/test/test1.java
@@ -0,0 +1,12 @@
+package java.cn.itcast.test;
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("222");
+        System.out.println("22");
+        System.out.println("11");
+        System.out.println("33");
+        System.out.println("88");
+    }
+}
